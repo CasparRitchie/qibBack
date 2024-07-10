0dea552be5bb40a439deb7dea39a3a34a058d48c
Caspar Ritchie <caspar.ritchie@lapiscine.pro>
2024-07-10 15:52:05 +0200

    Fix auth middleware initialization

--- server.js
+++ server.js
@@ -240,6 +240,11 @@ app.post('/login', async (req, res) => {
   }
 });
 
+// // Route to validate token **potentially needs adding
+// app.get('/validate-token', auth, (req, res) => {
+//   res.send({ user: req.user });
+// });
+
 // User Route to get logged-in user's data
 app.get('/user', auth, async (req, res) => {
   try {
